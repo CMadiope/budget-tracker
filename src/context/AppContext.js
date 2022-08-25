40cfd920059886103606252055b03cc41e58d8d6
Confidence Madiope <confidencem97@gmail.com>
2022-08-25 18:27:02 +0200

    updated AppContext with ADD_EXPENSE switch case

--- src/context/AppContext.js
+++ src/context/AppContext.js
@@ -13,6 +13,11 @@ export const AppContext = createContext();
 
 const AppReducer = (state, action) => {
   switch (action.type) {
+    case "ADD_EXPENSE":
+      return {
+        ...state,
+        expenses: [...state.expenses, action.payload],
+      };
     default:
       return state;
   }
