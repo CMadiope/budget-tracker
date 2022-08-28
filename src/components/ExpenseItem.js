0c1b4c52ddce209c941eb85e1ee2f4206dfff34c
Confidence Madiope <confidencem97@gmail.com>
2022-08-28 12:46:53 +0200

    .

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -16,9 +16,7 @@ const ExpenseItem = (props) => {
     <li className='list-group-item d-flex justify-content-between align-items-center'>
       {props.name}
       <div>
-        <span className='badge badge-primary badge-pill mr-3'>
-          R{props.cost}
-        </span>
+        <span className='badge bg-primary badge-pill mr-3'>R{props.cost}</span>
         <TiDelete size='1.5em' onClick={handleDeleteExpense}></TiDelete>
       </div>
     </li>
